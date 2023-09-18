868949898eb3616edce182938c2f052e4f89d305
Vitor Arimatea <vitorodev@outlook.com>
2023-09-18 10:30:35 -0300

    update: Deixando os componentes dinâmicos

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.css', './big-card.responsive.component.css']
 })
-export class BigCardComponent {
+export class BigCardComponent implements OnInit {
+
+	@Input()
+	cardImage: string = '';
+	@Input()
+	cardDate: string = '';
+	@Input()
+	cardTitle: string = '';
+	@Input()
+	cardText: string = '';
+
+	constructor() {
+
+	}
+
+	ngOnInit(): void {
+	}
 
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
   styleUrls: ['./small-card.component.css']
 })
-export class SmallCardComponent {
 
+export class SmallCardComponent {
+	@Input()
+	cardImage: string = '';
+	@Input()
+	cardDate: string = '';
+	@Input()
+	cardTitle: string = '';
 }
